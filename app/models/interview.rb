d8901081c9b266971126064bf54787ed567ea9ba
Ashish Upadhyay <ashish.cse2010@gmail.com>
2011-06-06 15:15:22 +0530

    Mani Review Changes 1

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,6 +1,7 @@
 class Interview < ActiveRecord::Base
   has_many :attachments
   validates_length_of :phoneNumber, :maximum=>10
+  validates_uniqueness_of :email  ,:phoneNumber ,:message => "Record Already Exists"
   validates_format_of :email,:with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i
   accepts_nested_attributes_for :attachments
   validates_presence_of :name ,:phoneNumber  ,:total_exp ,:ror_exp ,:doi ,:type_location ,:result,:email
